bb42147a18ef101249bf7d4637d1e315c50ae521
pubappbiz <ef+repository20160108@pubapp.biz>
2016-02-02 10:08:31 +0900

    refs #9 Load で設定ファイル (Config) を渡す

--- auth551_test.go
+++ auth551_test.go
@@ -1,13 +1,32 @@
 package auth551_test
 
 import (
-	"testing"
 	"github.com/go51/auth551"
+	"testing"
 )
 
 func TestLoad(t *testing.T) {
-	a1 := auth551.Load()
-	a2 := auth551.Load()
+	config := auth551.Config{
+		Form: auth551.ConfigForm{
+			LoginId: "",
+		},
+		Google: auth551.ConfigOAuth{
+			Vendor:       "google",
+			ClientId:     "Sample_ClientId",
+			ClientSecret: "Sample_ClientSecret",
+			RedirectUrl:  "https:sample.auth551.pubapp.biz",
+			Scope: []string{
+				"https://sample1.scope.pubapp.biz",
+				"https://sample2.scope.pubapp.biz",
+				"https://sample3.scope.pubapp.biz",
+			},
+			AuthUrl:  "https://auth.sample.auth551.pubapp.biz",
+			TokenUrl: "https://token.sample.auth551.pubapp.biz",
+		},
+	}
+
+	a1 := auth551.Load(&config)
+	a2 := auth551.Load(&config)
 
 	if a1 == nil {
 		t.Errorf("インスタンスの生成に失敗しました。")
@@ -21,8 +40,27 @@ func TestLoad(t *testing.T) {
 }
 
 func BenchmarkLoad(b *testing.B) {
+	config := auth551.Config{
+		Form: auth551.ConfigForm{
+			LoginId: "",
+		},
+		Google: auth551.ConfigOAuth{
+			Vendor:       "google",
+			ClientId:     "Sample_ClientId",
+			ClientSecret: "Sample_ClientSecret",
+			RedirectUrl:  "https:sample.auth551.pubapp.biz",
+			Scope: []string{
+				"https://sample1.scope.pubapp.biz",
+				"https://sample2.scope.pubapp.biz",
+				"https://sample3.scope.pubapp.biz",
+			},
+			AuthUrl:  "https://auth.sample.auth551.pubapp.biz",
+			TokenUrl: "https://token.sample.auth551.pubapp.biz",
+		},
+	}
+
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_ = auth551.Load()
+		_ = auth551.Load(&config)
 	}
 }
