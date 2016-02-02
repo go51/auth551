69a77de536561cdbefff82972e110e7c810c9ec9
pubappbiz <ef+repository20160108@pubapp.biz>
2016-02-02 11:13:43 +0900

    refs #11 AuthCodeUrl メソッドを実装する

--- auth551_test.go
+++ auth551_test.go
@@ -64,3 +64,60 @@ func BenchmarkLoad(b *testing.B) {
 		_ = auth551.Load(&config)
 	}
 }
+
+func TestAuthCodeUrl(t *testing.T) {
+	config := auth551.Config{
+		Form: auth551.ConfigForm{
+			LoginId: "",
+		},
+		Google: auth551.ConfigOAuth{
+			Vendor:       "google",
+			ClientId:     "Sample_ClientId",
+			ClientSecret: "Sample_ClientSecret",
+			RedirectUrl:  "https:sample.auth551.pubapp.biz",
+			Scope: []string{
+				"https://sample1.scope.pubapp.biz",
+				"https://sample2.scope.pubapp.biz",
+				"https://sample3.scope.pubapp.biz",
+			},
+			AuthUrl:  "https://auth.sample.auth551.pubapp.biz",
+			TokenUrl: "https://token.sample.auth551.pubapp.biz",
+		},
+	}
+
+	a := auth551.Load(&config)
+
+	url := a.AuthCodeUrl(auth551.VENDOR_GOOGLE)
+
+	if url != "https://auth.sample.auth551.pubapp.biz?access_type=offline&client_id=Sample_ClientId&redirect_uri=https%3Asample.auth551.pubapp.biz&response_type=code&scope=https%3A%2F%2Fsample1.scope.pubapp.biz+https%3A%2F%2Fsample2.scope.pubapp.biz+https%3A%2F%2Fsample3.scope.pubapp.biz" {
+		t.Errorf("URL の生成に失敗しました。")
+	}
+}
+
+func BenchmarkAuthCodeUrl(b *testing.B) {
+	config := auth551.Config{
+		Form: auth551.ConfigForm{
+			LoginId: "",
+		},
+		Google: auth551.ConfigOAuth{
+			Vendor:       "google",
+			ClientId:     "Sample_ClientId",
+			ClientSecret: "Sample_ClientSecret",
+			RedirectUrl:  "https:sample.auth551.pubapp.biz",
+			Scope: []string{
+				"https://sample1.scope.pubapp.biz",
+				"https://sample2.scope.pubapp.biz",
+				"https://sample3.scope.pubapp.biz",
+			},
+			AuthUrl:  "https://auth.sample.auth551.pubapp.biz",
+			TokenUrl: "https://token.sample.auth551.pubapp.biz",
+		},
+	}
+
+	a := auth551.Load(&config)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = a.AuthCodeUrl(auth551.VENDOR_GOOGLE)
+	}
+}
